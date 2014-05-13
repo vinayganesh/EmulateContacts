6689f894f9baf63dc3a9376447eed8f68ec3318f
unknown <vgan0001@PABET-CVF4VY1.sncrcorp.net>
2014-05-13 10:34:39 -0700

    Added the working solution

---
+++ EmulateContacts/MainPage.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using EmulateContacts.Resources;
+using System.IO;
+using Windows.Phone.PersonalInformation;
+using System.Xml.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace EmulateContacts
+{
+    public class RemoteContact
+    {
+        public string RemoteId { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string CodeName { get; set; }
+        public Stream photo { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format(" {0}\n {1}\n {2}\n {3}\n {4}\n {5}", RemoteId, GivenName, FamilyName, DisplayName, Email, CodeName);
+        }
+    }
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddContacts_Click(object sender, RoutedEventArgs e)
+        {
+            StatusTextBlock.Text = "Importing contacts...";
+           
+            CreateContacts();
+
+            StatusTextBlock.Text = "Done.";
+        }
+
+        private async void CreateContacts()
+        {
+            try
+            { 
+                int remoteId = 0;
+                int DN = 0;
+                int mail = 0;
+                int n;
+                n = int.Parse(txtNumberOfContacts.Text);
+
+                for (int i = 0; i < n; i++)
+                {
+                    Stream myStream = App.GetResourceStream(new Uri(@"Sample.jpg", UriKind.RelativeOrAbsolute)).Stream;
+                    var remoteContact = new RemoteContact
+                    {
+                        RemoteId = remoteId.ToString(),
+                        GivenName = "S",
+                        FamilyName = "NCR",
+                        DisplayName = "SNCR" + DN,
+                        Email = "Synchronoss" + mail + "@gmail.com",
+                        CodeName = "R",
+                        photo = myStream,
+                        
+                    };
+                    remoteId++; DN++; mail++;
+
+                    await AddContact(remoteContact);
+                }
+         }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        public async Task AddContact(RemoteContact remoteContact)
+        {
+            try 
+            { 
+                 ContactStore store = await ContactStore.CreateOrOpenAsync();
+
+                 StoredContact contact = new StoredContact(store);
+                 
+                 RemoteIdHelper remoteIDHelper = new RemoteIdHelper();
+                 contact.RemoteId = await remoteIDHelper.GetTaggedRemoteId(store, remoteContact.RemoteId);
+
+                 contact.GivenName = remoteContact.GivenName;
+                 contact.FamilyName = remoteContact.FamilyName;
+
+                 IDictionary<string, object> props = await contact.GetPropertiesAsync();
+                 props.Add(KnownContactProperties.Email, remoteContact.Email);
+
+                 IDictionary<string, object> extprops = await contact.GetExtendedPropertiesAsync();
+                 extprops.Add("Codename", remoteContact.CodeName);
+
+               
+                 if (remoteContact.DisplayName != null) contact.DisplayName = remoteContact.DisplayName;
+                 IInputStream I = remoteContact.photo.AsInputStream();
+                 await contact.SetDisplayPictureAsync(I);
+
+                 Debug.WriteLine(contact.Store);
+
+                 await contact.SaveAsync();
+                 Debug.WriteLine(String.Format("Adding:\n{0}", remoteContact.ToString()));
+            }
+
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+    }
+    
+}
+
---
+++ EmulateContacts/RemoteIdHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Phone.PersonalInformation;
+
+namespace EmulateContacts
+{
+    class RemoteIdHelper
+    {
+        // Key used to store the app's GUID
+        private const string ContactStoreLocalInstanceIdKey = "LocalInstanceId";
+
+        // Create a GUID and store it in the ContactStore's extended properties collection. If the GUID
+        // has already been set, it is not overwritten, so there is no danger in calling this method
+        // multiple times.
+        public async Task SetRemoteIdGuid(ContactStore store)
+        {
+            IDictionary<string, object> properties;
+            properties = await store.LoadExtendedPropertiesAsync().AsTask<IDictionary<string, object>>();
+            if (!properties.ContainsKey(ContactStoreLocalInstanceIdKey))
+            {
+                // The given store does not have a local instance id so set one against store extended properties
+                Guid guid = Guid.NewGuid();
+                properties.Add(ContactStoreLocalInstanceIdKey, guid.ToString());
+                System.Collections.ObjectModel.ReadOnlyDictionary<string, object> readonlyProperties = new System.Collections.ObjectModel.ReadOnlyDictionary<string, object>(properties);
+                await store.SaveExtendedPropertiesAsync(readonlyProperties).AsTask();
+            }
+        }
+
+        /// <summary>
+        /// Prepends the supplied remote ID with the GUID for the application in order to make sure
+        /// that the remote ID is unique across all applications.
+        /// </summary>
+        /// <param name="store">The app's contact store. The remote ID Guid is stored in the store's
+        /// extended properties collection.</param>
+        /// <param name="remoteId">The remote ID to be prepended with the app's GUID.</param>
+        /// <returns></returns>
+        public async Task<string> GetTaggedRemoteId(ContactStore store, string remoteId)
+        {
+            string taggedRemoteId = string.Empty;
+
+            System.Collections.Generic.IDictionary<string, object> properties;
+            properties = await store.LoadExtendedPropertiesAsync().AsTask<System.Collections.Generic.IDictionary<string, object>>();
+            if (properties.ContainsKey(ContactStoreLocalInstanceIdKey))
+            {
+                taggedRemoteId = string.Format("{0}_{1}", properties[ContactStoreLocalInstanceIdKey], remoteId);
+            }
+            else
+            {
+                // handle error condition
+            }
+
+            return taggedRemoteId;
+        }
+
+        /// <summary>
+        /// Removes the prepended GUID from the supplied remote ID.
+        /// </summary>
+        /// <param name="store">The app's contact store. The remote ID Guid is stored in the store's
+        /// extended properties collection.</param>
+        /// <param name="taggedRemoteId">The remote ID from which the app's GUID should be removed.</param>
+        /// <returns></returns>
+        public async Task<string> GetUntaggedRemoteId(ContactStore store, string taggedRemoteId)
+        {
+            string remoteId = string.Empty;
+
+            System.Collections.Generic.IDictionary<string, object> properties;
+            properties = await store.LoadExtendedPropertiesAsync().AsTask<System.Collections.Generic.IDictionary<string, object>>();
+            if (properties.ContainsKey(ContactStoreLocalInstanceIdKey))
+            {
+                string localInstanceId = properties[ContactStoreLocalInstanceIdKey] as string;
+                if (taggedRemoteId.Length > localInstanceId.Length + 1)
+                {
+                    remoteId = taggedRemoteId.Substring(localInstanceId.Length + 1);
+                }
+            }
+            else
+            {
+                // handle error condition
+            }
+
+            return remoteId;
+        }
+
+    }
+}
